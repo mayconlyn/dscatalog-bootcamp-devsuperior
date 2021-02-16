ef887f0698d5e1542cbd124e04bea07459f94118
Maycon Lyncoln <maycon.lyn@gmail.com>
2021-02-16 16:57:59 -0300

    Add Navbar

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import './core/assets/styles/custom.scss';
+import './app.scss';
+import Navbar from './core/assets/styles/components/Navbar';
 
 const App = () => {
     
 return (
-    <h1>App</h1>
+    <Navbar />
 );
-
 }
 
 export default App;
